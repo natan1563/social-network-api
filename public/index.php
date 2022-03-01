c9626432f7f95e997456a2e30028fb701d677e5c
natan1563 <natromafe@gmail.com>
2022-02-28 22:13:29 -0300

    Set default timezone

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+date_default_timezone_set('America/Sao_Paulo');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
